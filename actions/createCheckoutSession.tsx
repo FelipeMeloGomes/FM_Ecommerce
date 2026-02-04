c1ee21bd7c1c66eb9a3564b27c23f6ee55e11253
FelipeMeloGomes <felipemelogomes36@gmail.com>
2026-02-04 16:58:04 -0300

    fix: adjust create checkout session

--- actions/createCheckoutSession.tsx
+++ actions/createCheckoutSession.tsx
@@ -10,7 +10,7 @@ export interface Metadata {
   orderNumber: string;
   customerName: string;
   customerEmail: string;
-  clerkUserId?: string | undefined;
+  clerkUserId?: string;
   address?: Address | null;
 }
 
@@ -24,32 +24,35 @@ export async function createCheckoutSession(
   metadata: Metadata,
 ) {
   try {
+    // Retrieve existing customer or create a new one
     const customers = await stripe.customers.list({
       email: metadata.customerEmail,
       limit: 1,
     });
     const customerId = customers?.data?.length > 0 ? customers.data[0].id : "";
 
-    const sessionPayLoad: Stripe.Checkout.SessionCreateParams = {
+    const sessionPayload: Stripe.Checkout.SessionCreateParams = {
       metadata: {
         orderNumber: metadata.orderNumber,
         customerName: metadata.customerName,
         customerEmail: metadata.customerEmail,
         clerkUserId: metadata.clerkUserId!,
-        address: JSON.stringify(metadata?.address),
+        address: JSON.stringify(metadata.address),
       },
       mode: "payment",
       allow_promotion_codes: true,
       payment_method_types: ["card"],
-      invoice_creation: { enabled: true },
+      invoice_creation: {
+        enabled: true,
+      },
       success_url: `${
         process.env.NEXT_PUBLIC_BASE_URL
       }/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`,
       cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/cart`,
       line_items: items?.map((item) => ({
         price_data: {
-          currency: "BRL",
-          unit_amount: Math.round(item.product.price! * 100),
+          currency: "USD",
+          unit_amount: Math.round(item?.product?.price! * 100),
           product_data: {
             name: item?.product?.name || "Unknown Product",
             description: item?.product?.description,
@@ -64,15 +67,15 @@ export async function createCheckoutSession(
       })),
     };
     if (customerId) {
-      sessionPayLoad.customer = customerId;
+      sessionPayload.customer = customerId;
     } else {
-      sessionPayLoad.customer_email = metadata.customerEmail;
+      sessionPayload.customer_email = metadata.customerEmail;
     }
 
-    const session = await stripe.checkout.sessions.create(sessionPayLoad);
+    const session = await stripe.checkout.sessions.create(sessionPayload);
     return session.url;
   } catch (error) {
-    console.error("Error creating Checkout Session:", error);
+    console.error("Error creating Checkout Session", error);
     throw error;
   }
 }
