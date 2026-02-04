da42ab677f58ec59f8a779818ec1b5ab357e20e9
FelipeMeloGomes <felipemelogomes36@gmail.com>
2026-02-04 15:27:01 -0300

    feat: building adjust

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,25 @@
-import { currentUser } from "@clerk/nextjs/server";
-import CartIcon from "./CartIcon";
-import Container from "./Container";
-import FavoriteButton from "./FavoriteButton";
-import HeaderMenu from "./HeaderMenu";
-import Logo from "./Logo";
-import MobileMenu from "./MobileMenu";
-import SearchBar from "./SearchBar";
-import SignIn from "./SignIn";
+import Link from "next/link";
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { ClerkLoaded, SignedIn, UserButton } from "@clerk/nextjs";
+import { Logs } from "lucide-react";
+import { getMyOrders } from "@/sanity/queries";
+import Container from "./Container";
+import Logo from "./Logo";
+import HeaderMenu from "./HeaderMenu";
+import SearchBar from "./SearchBar";
+import CartIcon from "./CartIcon";
+import FavoriteButton from "./FavoriteButton";
+import SignIn from "./SignIn";
+import MobileMenu from "./MobileMenu";
 
 const Header = async () => {
   const user = await currentUser();
+  const { userId } = await auth();
+
+  let orders = null;
+  if (userId) {
+    orders = await getMyOrders(userId);
+  }
 
   return (
     <header className="sticky top-0 z-50 py-5 bg-white/70 backdrop-blur-md">
@@ -24,6 +33,18 @@ const Header = async () => {
           <SearchBar />
           <CartIcon />
           <FavoriteButton />
+
+          {user && (
+            <Link
+              href={"/orders"}
+              className="group relative hover:text-shop_light_green hoverEffect"
+            >
+              <Logs />
+              <span className="absolute -top-1 -right-1 bg-shop_btn_dark_green text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center">
+                {orders?.length ? orders?.length : 0}
+              </span>
+            </Link>
+          )}
 
           <ClerkLoaded>
             <SignedIn>
