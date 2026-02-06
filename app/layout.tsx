d348c00a662b70f743a43b2e31dfbf5d6feeaf1e
FelipeMeloGomes <felipemelogomes36@gmail.com>
2026-02-06 13:46:47 -0300

    feat: add speend insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body className="font-poppins antialiased">
         {children}
+        <SpeedInsights />
         <Toaster
           position="bottom-right"
           toastOptions={{
