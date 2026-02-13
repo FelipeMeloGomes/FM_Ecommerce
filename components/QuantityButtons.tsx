94c1470348fdd876f2cf427159ad4d7a4dd8fd36
FelipeMeloGomes <felipemelogomes36@gmail.com>
2026-02-13 13:57:06 -0300

    chore(ui): simplify cart toast messages

--- components/QuantityButtons.tsx
+++ components/QuantityButtons.tsx
@@ -17,18 +17,18 @@ const QuantityButtons = ({ product, className }: Props) => {
   const handleRemoveProduct = () => {
     removeItem(product?._id);
     if (itemCount > 1) {
-      toast.success("Quantity Decreased successfully!");
+      toast.success("Quantidade atualizada");
     } else {
-      toast.success(`${product?.name?.substring(0, 12)} removed successfully!`);
+      toast.success(`${product?.name?.substring(0, 12)} Produto removido`);
     }
   };
 
   const handleAddToCart = () => {
     if ((product?.stock as number) > itemCount) {
       addItem(product);
-      toast.success("Quantity Increased successfully!");
+      toast.success("Quantidade atualizada");
     } else {
-      toast.error("Can not add more than available stock");
+      toast.error("Estoque insuficiente");
     }
   };
 
