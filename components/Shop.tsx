bb100c48720e0816de52136ae7211e45ac31a0a0
FelipeMeloGomes <felipemelogomes36@gmail.com>
2026-04-13 18:27:41 -0300

    Revert "style: increase gap between product cards"
    This reverts commit 1fbbf263de031bde0f2999229c258f70fdd8e2e3.

--- components/Shop.tsx
+++ components/Shop.tsx
@@ -281,7 +281,7 @@ const Shop = memo(
                   </div>
                 ) : products?.length > 0 ? (
                   <>
-                    <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+                    <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
                       {products?.map((product) => (
                         <ProductCard
                           key={product?._id}
