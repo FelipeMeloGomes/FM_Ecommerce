cd870afd88ef28b9876573065b51868f989c42ca
FelipeMeloGomes <felipemelogomes36@gmail.com>
2026-06-09 13:55:02 -0300

    feat: add preconnect hints and dns-prefetch for Sanity and Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-import { Bricolage_Grotesque, DM_Sans } from "next/font/google";
-import { SanityLive } from "@/sanity/lib/live";
-import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Bricolage_Grotesque, DM_Sans } from "next/font/google";
 import { ClerkThemeProvider } from "@/components/ClerkThemeProvider";
 import { GlobalErrorWrapper } from "@/components/GlobalErrorWrapper";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "@/components/Toaster";
+import { SanityLive } from "@/sanity/lib/live";
 
 const displayFont = Bricolage_Grotesque({
   subsets: ["latin"],
@@ -24,6 +23,16 @@ const bodyFont = DM_Sans({
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
+      <head>
+        <link
+          rel="preconnect"
+          href="https://cdn.sanity.io"
+          crossOrigin="anonymous"
+        />
+        <link rel="preconnect" href="https://clerk.accounts.dev" />
+        <link rel="dns-prefetch" href="https://cdn.sanity.io" />
+        <link rel="dns-prefetch" href="https://clerk.accounts.dev" />
+      </head>
       <body
         className={`${displayFont.variable} ${bodyFont.variable} font-sans antialiased`}
       >
